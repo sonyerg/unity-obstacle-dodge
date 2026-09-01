e844618b521ae2d9ea3e89e5d0dc73bc17275307
sonyerg <ericksonrguinto@gmail.com>
2026-09-02 07:46:48 +1200

    initial changes

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    float xValue = 0.01f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(xValue, 0f, 0f);
     }
 }
