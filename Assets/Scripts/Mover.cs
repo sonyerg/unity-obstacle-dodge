adc1c992f0aa627011475a5991f3992fc4a42a2c
sonyerg <ericksonrguinto@gmail.com>
2026-09-03 18:40:17 +1200

    added Time.deltaTime * moveSpeed

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField]float moveSpeed = 10f;
+
     void Start()
     {
         
@@ -9,9 +11,11 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
+        // Using Time.deltaTime Unity can tell us how long each frame took to execute.
+        // Multiplying to Time.deltaTime makes the game "frame rate independent".
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0.0f;
-        float zValue = Input.GetAxis("Vertical");
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, yValue, zValue);
     }
